b87cb77908163d1cb7858fcb229ed9c6c2baf5cf
mrmurphy98 <alireza.real98@gmail.com>
2018-10-31 13:00:27 +0330

    AddHtmlImport

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Import Excel into Database</title>
+</head>
+<body>
+    <div id="mainWrapper">
+        <form method="post" action="index.php" enctype="multipart/form-data">
+            <br>
+            <input type="file" name="file"/>
+            <br>
+            <input type="submit" name="submit" value="submit"/>
+        </form>
+    </div>
+</body>
+</html>
+
 <?php 
 echo 'hi'; 
 echo 'test';
