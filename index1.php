9f195b6abad8e8f04fa0f8db9a04900fc304596a
ruhollahm8 <graphist77@gmail.com>
2018-11-01 23:31:21 +0330

    add style

--- index1.php
+++ index1.php
@@ -1,30 +1,28 @@
 <?php 
 
-$servername = "localhost";
-$username = "root";
-$password = "admin";
+if( isset($_POST['submit']) ){
+    $servername = "localhost";
+    $username = "root";
+    $password = "admin";
+    $dbname = $_POST['dbname'];
 
-try {
-    $conn = new PDO("mysql:host=$servername", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE DATABASE IF NOT EXISTS musicDB";
-    $conn->exec($sql);
-    $sql = "use musicDB";
-    $conn->exec($sql);
-    $sql = "CREATE TABLE IF NOT EXISTS ARTISTS (
-                ID int(11) AUTO_INCREMENT PRIMARY KEY,
-                artistname varchar(30) NOT NULL)";
-    $conn->exec($sql);
-    echo "DB created successfully";
-}
-catch(PDOException $e)
-{
-    echo $sql . "<br>" . $e->getMessage();
-}
-
-
-if(isset($_POST['submit'])){
-    echo "true";
+    try {
+        $conn = new PDO("mysql:host=$servername", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+        $conn->exec($sql);
+        $sql = "use $dbname";
+        $conn->exec($sql);
+        $sql = "CREATE TABLE IF NOT EXISTS ARTISTS (
+                    ID int(11) AUTO_INCREMENT PRIMARY KEY,
+                    artistname varchar(30) NOT NULL)";
+        $conn->exec($sql);
+        echo "DB created successfully";
+    }
+    catch(PDOException $e)
+    {
+        echo $sql . "<br>" . $e->getMessage();
+    }
 
 }
 ?>
